d3a5377e3a163abd3c98149b5f84f7f1d334566d
Manal <manal.hebbal@student.vinci.be>
2022-12-23 22:26:10 +0100

    creation utils + test uppercase

---
+++ __tests__/planetNameIsCorrect.test.js
@@ -0,0 +1,9 @@
+const { planetNameIsCorrect } = require('../utils/utils');
+
+test('test if the name is in uppercase', () => {
+    expect(planetNameIsCorrect('TRApIST')).toBe(false);
+});
+
+test('test if the name is in uppercase', () => {
+    expect(planetNameIsCorrect('SATURNE')).toBe(true);
+});
---
+++ utils/utils.js
@@ -0,0 +1,3 @@
+module.exports.planetNameIsCorrect = (planetName) => {
+    //regex
+};
